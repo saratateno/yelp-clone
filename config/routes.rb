f565a28e60e6320ab0de406962bc02fbe2c1c4e0
Sara Tateno <sara.tateno@gmail.com>
2016-01-20 11:52:39 +0000

    Adds Omniauth Facebook login and dotenv

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
+
   resources :restaurants do
     resources :reviews
   end
 
   root to: "restaurants#index"
+
 
   # get 'restaurants' => 'restaurants#index'
 
